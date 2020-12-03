03d7d3dbc43e941180af2adf866982600ef60b7c
Jordan Samhi <j.samhi@me.com>
2020-12-03 08:34:59 +0100

    Add: inner local finder class

--- src/main/java/lu/uni/trux/difuzer/instrumentation/UnitGenerator.java
+++ src/main/java/lu/uni/trux/difuzer/instrumentation/UnitGenerator.java
@@ -5,16 +5,22 @@ import java.util.List;
 
 import lu.uni.trux.difuzer.managers.SourcesSinksManager;
 import lu.uni.trux.difuzer.utils.Constants;
-import lu.uni.trux.difuzer.utils.LocalFinder;
 import lu.uni.trux.difuzer.utils.Utils;
 import soot.SootMethod;
 import soot.SootMethodRef;
 import soot.Type;
 import soot.Unit;
 import soot.Value;
+import soot.jimple.AbstractStmtSwitch;
+import soot.jimple.AssignStmt;
 import soot.jimple.ConditionExpr;
+import soot.jimple.Constant;
 import soot.jimple.IfStmt;
+import soot.jimple.InstanceInvokeExpr;
+import soot.jimple.InvokeExpr;
 import soot.jimple.Jimple;
+import soot.jimple.Stmt;
+import soot.toolkits.graph.BriefUnitGraph;
 
 /*-
  * #%L
@@ -89,5 +95,57 @@ public class UnitGenerator {
 		Unit u = Jimple.v().newInvokeStmt(Jimple.v().newStaticInvokeExpr(
 				ref, args));
 		return u;
+	}
+
+	public class LocalFinder {
+
+		private SootMethod method;
+
+		public LocalFinder(SootMethod sm) {
+			this.method = sm;
+		}
+
+		public List<Value> findBooleanOrigin(Value v, Stmt  stmt) {
+			List<Value> locals = new ArrayList<Value>();
+			BriefUnitGraph bug = new BriefUnitGraph(method.retrieveActiveBody());
+			for(Unit pred : bug.getPredsOf(stmt)) {
+				this.getLocalsFromBoolean(pred, v, new ArrayList<Unit>(), locals, bug);
+			}
+			return locals;
+		}
+
+		private void getLocalsFromBoolean(Unit u, final Value v, final List<Unit> visitedUnits, final List<Value> locals, BriefUnitGraph bug) {
+			visitedUnits.add(u);
+			u.apply(new AbstractStmtSwitch() {
+				public void caseAssignStmt(AssignStmt stmt) {
+					Value leftOp = stmt.getLeftOp();
+					if(leftOp.equals(v)) {
+						Value rightOp = stmt.getRightOp();
+						if(rightOp instanceof InvokeExpr) {
+							InvokeExpr ie = (InvokeExpr) rightOp;
+							if(ie instanceof InstanceInvokeExpr) {
+								InstanceInvokeExpr iie = (InstanceInvokeExpr) ie;
+								Value base = iie.getBase();
+								if(!locals.contains(base)) {
+									locals.add(base);
+								}
+							}
+							for(Value arg : ie.getArgs()) {
+								if(!(arg instanceof Constant)) {
+									if(!locals.contains(arg)) {
+										locals.add(arg);
+									}
+								}
+							}
+						}
+					}
+				}
+			});
+			for(Unit pred : bug.getPredsOf(u)) {
+				if(!visitedUnits.contains(pred)) {
+					this.getLocalsFromBoolean(pred, v, visitedUnits, locals, bug);
+				}
+			}
+		}
 	}
 }
