9fd780b6c3de4909ccfb85940ef8e0715058c9da
Jordan Samhi <j.samhi@me.com>
2020-12-04 08:34:39 +0100

    Add: method if call + list of variables used

--- src/main/java/lu/uni/trux/difuzer/triggers/TriggerIfCall.java
+++ src/main/java/lu/uni/trux/difuzer/triggers/TriggerIfCall.java
@@ -1,11 +1,15 @@
 package lu.uni.trux.difuzer.triggers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lu.uni.trux.difuzer.utils.Constants;
 import soot.SootMethod;
 import soot.Unit;
+import soot.Value;
+import soot.jimple.Constant;
 import soot.jimple.IfStmt;
+import soot.jimple.InvokeExpr;
 import soot.jimple.InvokeStmt;
 import soot.jimple.infoflow.solver.cfg.InfoflowCFG;
 
@@ -38,15 +42,34 @@ import soot.jimple.infoflow.solver.cfg.InfoflowCFG;
 public class TriggerIfCall extends Trigger {
 
 	private List<SootMethod> sources;
+	private List<Value> variablesUsedInCondition;
+	private Unit ifMethodCall;
 	
 	public TriggerIfCall(Unit u, InfoflowCFG icfg, List<SootMethod> sources) {
 		super();
 		this.setIcfg(icfg);
 		IfStmt i = this.generateCondition(u, icfg);
+		this.setIfMethodCall(u);
+		this.variablesUsedInCondition = new ArrayList<Value>();
+		this.generateListOfVariablesUsed(u);
 		this.setCondition(i);
 		this.generateGraph();
 		this.generateGuardedStmts();
 		this.setSources(sources);
+	}
+
+	private void generateListOfVariablesUsed(Unit u) {
+		if(u instanceof InvokeStmt) {
+			InvokeStmt inv = (InvokeStmt) u;
+			InvokeExpr ie = inv.getInvokeExpr();
+			if(ie.getMethod().getName().equals(Constants.IF_METHOD)) {
+				for(Value v: ie.getArgs()) {
+					if(!(v instanceof Constant)) {
+						this.variablesUsedInCondition.add(v);
+					}
+				}
+			}
+		}
 	}
 
 	private IfStmt generateCondition(Unit u, InfoflowCFG icfg) {
@@ -69,5 +92,21 @@ public class TriggerIfCall extends Trigger {
 
 	public void setSources(List<SootMethod> sources) {
 		this.sources = sources;
+	}
+
+	public List<Value> getVariablesUsedInCondition() {
+		return variablesUsedInCondition;
+	}
+
+	public void setVariablesUsedInCondition(List<Value> variablesUsedInCondition) {
+		this.variablesUsedInCondition = variablesUsedInCondition;
+	}
+
+	public Unit getIfMethodCall() {
+		return ifMethodCall;
+	}
+
+	public void setIfMethodCall(Unit ifMethodCall) {
+		this.ifMethodCall = ifMethodCall;
 	}
 }
